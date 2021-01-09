813f4a00690a3eae792d7a74a5e91152c059f299
Michael Collins <michael_collins@applied-science.com>
2021-01-09 12:13:06 -0800

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,10 +8,10 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const location = search.value;
 
-    messageOne.textContent = 'Chrome Javascript';
-    messageTwo.textContent = 'xxx';
+    messageOne.textContent = '';
+    messageTwo.textContent = '';
 
-    const url = 'http://localhost:3000/weather?address=' + encodeURI(location);
+    const url = '/weather?address=' + encodeURI(location);
     fetch(url).then((response) => {
     
             response.json().then((data) => {
--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ const { response } = require('express');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
 const partialsPath = path.join(__dirname, '../templates/partials');
@@ -110,6 +112,6 @@ app.get('*', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log('Server is up on port ' + port);
 });
